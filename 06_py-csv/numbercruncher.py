693a582958925e9c550e247f83da4d51b2b64178
RussellGoychayev <rgoychayev30@stuy.edu>
2022-09-30 13:18:10 -0400

    finished 06

---
+++ 06_py-csv/numbercruncher.py
@@ -0,0 +1,28 @@
+'''
+heading
+
+qcc:
+disco:
+ops sum:
+'''
+import random
+
+a = open("occupations.csv")
+b = a.read()
+
+dict = {}
+txt = b.split("\n")
+txt.pop()#remove last element: " "
+txt.pop()#remove last element: "'Total': '99.8'"
+txt.pop(0)#remove first element: "'Job Class': 'Percentage'"
+
+for x in txt:
+    list = x.rsplit(",", 1)
+    dict[list[0]]= float(list[1])
+
+list = []
+for x in dict:
+    for y in range(0, int(dict[x]*10)):
+        list.append(x)
+
+#print(random.choice(list))
