48c7ac477aa09b10d55de19b96c55282dde6b140
Russell Goychayev <rgoychayev30@stuy.edu>
2022-11-22 20:38:05 -0500

    finished k20 on 11-22-2022 22:37

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import urllib.request, json
 
 app = Flask(__name__)
@@ -8,11 +8,14 @@ def picture():
     #read key from file
     key = open("key_nasa.txt").read()
     #append to url string
-    urlString = "https://api.nasa.gov/planetary/apod?api_key=" + key
+    url_string = "https://api.nasa.gov/planetary/apod?api_key=" + key
     #make HTTP request to API using key
-    jsonurl = urllib.request.urlopen(urlString)
-    text = json.loads(jsonurl.read()) # <-- read from it    print(dict)
-    return "hello"
+    jsonurl = urllib.request.urlopen(url_string)
+    #store JSON file as text
+    text = json.loads(jsonurl.read())
+    #get src for main.html
+    src_string = text["url"]
+    return render_template("main.html", src = src_string)
 
 if __name__ == '__main__':
     app.debug = True
